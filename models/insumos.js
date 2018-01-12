e63dea1ce2483697f6734e05f08016e0eca73ca3
Eduardo Grosclaude <eduardo.grosclaude@gmail.com>
2018-01-12 14:35:39 -0300

    mc

---
+++ models/insumos.js
@@ -0,0 +1,30 @@
+// insumos
+
+
+var sql = require('mongoose');
+
+var insumosSchema = mongoose.Schema({
+nombre: String,
+unidades: String,
+firstName: String,
+lastName: String,
+email: String,
+address1: String,
+address2: String,
+city: String,
+state: String,
+zip: String,
+phone: String,
+salesNotes: [{
+date: Date,
+salespersonId: Number,
+notes: String,
+}],
+});
+
+
+customerSchema.methods.getOrders = function(){
+return Orders.find({ customerId: this._id });
+};
+
+module.exports = mongoose.model('insumos', insumosSchema);
